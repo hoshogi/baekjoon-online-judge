143a0fbff66a2de222b2e1d82b7270f9a37df425
hoshogi <hoshogi@gmail.com>
2021-01-16 23:50:35 +0900

    BOJ_10869: 사칙연산

---
+++ 10869/10869.cpp
@@ -0,0 +1,25 @@
+/*
+Title	: BOJ_10869 [사칙연산]
+Author	: Hoseok Lee
+Date	: 2020/01/13
+
+https://www.acmicpc.net/problem/10869
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+
+	cin >> a >> b;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+
+	return 0;
+}
