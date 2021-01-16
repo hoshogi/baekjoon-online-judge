a81fc8a81e258b3a7de49ad33cda5e56e0b17dbe
hoshogi <hoshogi@gmail.com>
2021-01-16 23:48:54 +0900

    BOJ_1008: A/B

---
+++ 1008/1008.cpp
@@ -0,0 +1,21 @@
+/*
+Title	: BOJ_1008 [A/B]
+Author	: Hoseok Lee
+Date	: 2021/01/13
+
+https://www.acmicpc.net/problem/1008
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+
+	cin >> a >> b;
+	printf("%.9lf \n", (double)a / (double)b);
+
+	return 0;
+}
