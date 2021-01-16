272579abb7d9a8ed0bcd0e14ace4c4eeb2ce2684
hoshogi <hoshogi@gmail.com>
2021-01-16 23:58:35 +0900

    BOJ_1550: 16진수

---
+++ 1550/1550.cpp
@@ -0,0 +1,21 @@
+/*
+Title	: BOJ_1550 [16Áø¼ö]
+Author	: Hoseok Lee
+Date	: 2021/01/15
+
+https://www.acmicpc.net/problem/1550
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n;
+
+	scanf("%x", &n);
+	cout << n << endl;
+
+	return 0;
+}
