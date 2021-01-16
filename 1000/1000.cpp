b517ca246af40c28a11128edd1f5219002b6978e
hoshogi <hoshogi@gmail.com>
2021-01-16 23:46:46 +0900

    BOJ_1000: A+B

---
+++ 1000/1000.cpp
@@ -0,0 +1,21 @@
+/*
+Title	: BOJ_1000 [A+B]
+Author	: Hoseok Lee
+Date	: 2021/01/12
+
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/1000
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+
+	cin >> a >> b;
+	cout << a + b;
+
+	return 0;
+}
