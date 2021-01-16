c572acf365c706dd8832f11ca3d41da2469b0aaa
hoshogi <hoshogi@gmail.com>
2021-01-16 23:53:19 +0900

    BOJ_10998: AxB

---
+++ 10998/10998.cpp
@@ -0,0 +1,21 @@
+/*
+Title	: BOJ_10998 [AxB]
+Author	: Hoseok Lee
+Date	: 2021/01/14
+
+https://www.acmicpc.net/problem/10998
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+
+	cin >> a >> b;
+	cout << a * b << endl;
+
+	return 0;
+}
