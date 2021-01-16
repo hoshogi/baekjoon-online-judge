50581f7db0cf7ba9c72482e19d321d9548f625e1
hoshogi <hoshogi@gmail.com>
2021-01-16 23:48:08 +0900

    BOJ_2557: Hello World

---
+++ 2557/2557.cpp
@@ -0,0 +1,18 @@
+/*
+Title	: BOJ_2557 [Hello World]
+Author	: Hoseok Lee
+Date	: 2021/01/13
+
+https://www.acmicpc.net/problem/2557
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "Hello World!" << endl;
+	
+	return 0;
+}
