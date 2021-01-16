776dd7b1e5a7fc1eff379f4ada54c292b903363e
hoshogi <hoshogi@gmail.com>
2021-01-16 23:47:20 +0900

    BOJ_1001: A-B

---
+++ 1001/1001.cpp
@@ -0,0 +1,21 @@
+/*
+Title	: BOJ_1001 [A-B]
+Author	: Hoseok Lee
+Date	: 2021/01/12
+
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/1001
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+
+	cin >> a >> b;
+	cout << a - b;
+
+	return 0;
+}
