b0fad49dc27286da814bc1710eefaf03c3bac3ee
hoshogi <hoshogi@gmail.com>
2021-01-16 23:53:49 +0900

    BOJ_11654: 아스키 코드

---
+++ 11654/11654.cpp
@@ -0,0 +1,21 @@
+/*
+Title	: BOJ_11654 [아스키 코드]
+Author	: Hoseok Lee
+Date	: 2021/01/14
+
+https://www.acmicpc.net/problem/11654
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	char c;
+	
+	cin >> c;
+	printf("%d", c);
+
+	return 0;
+}
