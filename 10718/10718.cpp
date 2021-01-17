544827b00abe7da28a3b408ccdeb73afc83fb683
hoshogi <hoshogi@gmail.com>
2021-01-17 15:01:22 +0900

    BOJ_10718: We love kriii

---
+++ 10718/10718.cpp
@@ -0,0 +1,19 @@
+/*
+Title	: BOJ_10718 [We love kriii]
+Author	: Hoseok Lee
+Date	: 2021/01/17
+
+https://www.acmicpc.net/problem/10718
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "강한친구 대한육군" << endl;
+	cout << "강한친구 대한육군" << endl;
+
+	return 0;
+}
