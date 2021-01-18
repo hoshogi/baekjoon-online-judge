1b98633bc181ecd6866036ca1fc2d8b39a9e5238
hoshogi <hoshogi@gmail.com>
2021-01-18 13:45:20 +0900

    BOJ_10797: 10부제

---
+++ 10797/10797.cpp
@@ -0,0 +1,27 @@
+﻿/*
+Title	: BOJ_10797 [10부제]
+Author	: Hoseok Lee
+Date	: 2021/01/18
+
+https://www.acmicpc.net/problem/10797
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int day, num[5], count = 0;
+
+	cin >> day;
+	for (int i = 0;i < 5;i++)
+	{
+		cin >> num[i];
+		if (day == num[i])
+			count++;
+	}
+
+	cout << count << endl;
+	return 0;
+}
