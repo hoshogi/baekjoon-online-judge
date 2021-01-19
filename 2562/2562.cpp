4d4e72c77958f618f9bc768521346f031345f9dc
hoshogi <hoshogi@gmail.com>
2021-01-19 23:12:46 +0900

    BOJ_2562: 최댓값

---
+++ 2562/2562.cpp
@@ -0,0 +1,32 @@
+﻿/*
+Title	: BOJ_2562 [최댓값]
+Author	: Hoseok Lee
+Date	: 2021/01/19
+
+https://www.acmicpc.net/problem/2562
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int num[9], max_num = 0, max_index;
+
+	for (int i = 0;i < 9;i++)
+	{
+		cin >> num[i];
+
+		if (num[i] > max_num)
+		{
+			max_num = num[i];
+			max_index = i+1;
+		}
+	}
+	
+	cout << max_num << endl;
+	cout << max_index << endl;
+
+	return 0;
+}
