d6502d23c1f7f22adb9e806edd148bcbd2f25222
hoshogi <hoshogi@gmail.com>
2021-01-20 23:12:56 +0900

    BOJ_2558: A+B - 2

---
+++ 2558/2558.cpp
@@ -0,0 +1,21 @@
+/*
+Title   : BOJ_2558 [A+B - 2]
+Author  : Hoseok Lee
+Date    : 2021/01/20
+
+https://www.acmicpc.net/problem/2558
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+
+	cin >> a >> b;
+	cout << a + b << endl;
+
+	return 0;
+}
