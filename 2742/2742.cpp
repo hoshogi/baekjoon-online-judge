8ba262f1e0d3c28b234e30108266150efd3c100a
hoshogi <hoshogi@gmail.com>
2021-01-20 22:41:40 +0900

    BOJ_2742: 기찍 N

---
+++ 2742/2742.cpp
@@ -0,0 +1,27 @@
+﻿/*
+Title   : BOJ_2742 [기찍 N]
+Author  : Hoseok Lee
+Date    : 2021/01/20
+
+https://www.acmicpc.net/problem/2742
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+
+    cin >> n;
+    for (int i = n;i >= 1;i--)
+    {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
