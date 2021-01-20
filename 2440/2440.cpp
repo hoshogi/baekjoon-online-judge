787487bd1bf6d2d0f301a01a2ba3ae83650c137f
hoshogi <hoshogi@gmail.com>
2021-01-20 23:20:03 +0900

    BOJ_2440: 별 찍기 - 3

---
+++ 2440/2440.cpp
@@ -0,0 +1,26 @@
+﻿/*
+Title   : BOJ_2440 [별 찍기 - 3]
+Author  : Hoseok Lee
+Date    : 2021/01/20
+
+https://www.acmicpc.net/problem/2440
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n;
+
+	cin >> n;
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = i;j < n;j++)
+			cout << "*";
+		cout << endl;
+	}
+
+	return 0;
+}
