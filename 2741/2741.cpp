d00b713137cc8dc904f7a605830913394d49d41a
hoshogi <hoshogi@gmail.com>
2021-01-20 22:12:06 +0900

    BOJ_2741: N 찍기

---
+++ 2741/2741.cpp
@@ -0,0 +1,27 @@
+﻿/*
+Title   : BOJ_2741 [N 찍기]
+Author  : Hoseok Lee
+Date    : 2021/01/20
+
+https://www.acmicpc.net/problem/2741
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+
+    cin >> n;
+    for (int i = 1;i <= n;i++)
+    {
+        cout << i << "\n";
+    }
+
+    return 0;
+}
