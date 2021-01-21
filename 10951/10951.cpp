33d9606ecf930c1e032d52e8dcbd4895681374e7
hoshogi <hoshogi@gmail.com>
2021-01-21 22:26:18 +0900

    BOJ_10951: A+B - 4

---
+++ 10951/10951.cpp
@@ -0,0 +1,24 @@
+﻿/*
+Title   : BOJ_10951 [A+B - 4]
+Author  : Hoseok Lee
+Date    : 2021/01/21
+
+https://www.acmicpc.net/problem/10951
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a, b;
+
+    // EOF를 입력하면 종료
+    while (cin >> a >> b)
+    {
+        cout << a + b << endl;
+    }
+
+    return 0;
+}
