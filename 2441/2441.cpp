3088074f4b0365ab64a8479a96114d8e33f916b4
hoshogi <hoshogi@gmail.com>
2021-01-21 22:17:04 +0900

    BOJ_2441: 별 찍기 - 4

---
+++ 2441/2441.cpp
@@ -0,0 +1,28 @@
+﻿/*
+Title   : BOJ_2441 [별 찍기 - 4]
+Author  : Hoseok Lee
+Date    : 2021/01/21
+
+https://www.acmicpc.net/problem/2441
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n;
+
+    cin >> n;
+    for (int i = 0;i < n;i++)
+    {
+        for (int j = 0;j < i;j++)
+            cout << " ";
+        for (int j = n;j > i;j--)
+            cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
