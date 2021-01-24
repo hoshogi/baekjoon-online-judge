2042f75c90a377fbe6772aa114da93f29eca3d6e
hoshogi <hoshogi@gmail.com>
2021-01-24 23:33:30 +0900

    BOJ_7287: 등록

---
+++ 7287/7287.cpp
@@ -0,0 +1,19 @@
+﻿/*
+Title	: BOJ_7287 [등록]
+Author	: Hoseok Lee
+Date	: 2021/01/24
+
+https://www.acmicpc.net/problem/7287
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << 52 << endl;
+	cout << "hoshogiii" << endl;
+
+	return 0;
+}
