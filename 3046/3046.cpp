1cbb26ec38effc2b15486b8ac9a93b67245addae
hoshogi <hoshogi@gmail.com>
2021-01-25 23:50:29 +0900

    BOJ_3046: R2

---
+++ 3046/3046.cpp
@@ -0,0 +1,21 @@
+/*
+Title	: BOJ_3046 [R2]
+Author	: Hoseok Lee
+Date	: 2021/01/25
+
+https://www.acmicpc.net/problem/3046
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int r, s;
+
+    cin >> r >> s;
+    cout << 2 * s - r << endl;
+
+    return 0;
+}
