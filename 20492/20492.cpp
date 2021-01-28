e8bb4cad4af3bbfc3a97883f2125c37cd94c3ca4
hoshogi <hoshogi@gmail.com>
2021-01-28 16:41:20 +0900

    BOJ_20492: 세금

---
+++ 20492/20492.cpp
@@ -0,0 +1,21 @@
+﻿/*
+Title	: BOJ_20492 [세금]
+Author	: Hoseok Lee
+Date	: 2021/01/28
+
+https://www.acmicpc.net/problem/20492
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int prize;
+
+	cin >> prize;
+	printf("%d %d\n",(int)(prize * 0.78), (int)(prize * (0.8 + 0.2 * 0.78)));
+
+	return 0;
+}
