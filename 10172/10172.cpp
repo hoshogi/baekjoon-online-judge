cf4bf8298f8475551a756b5d7b3c9caaae2325c7
hoshogi <hoshogi@gmail.com>
2021-01-30 12:05:38 +0900

    BOJ_10172: 개

---
+++ 10172/10172.cpp
@@ -0,0 +1,21 @@
+﻿/*
+Title	: BOJ_10172 [개]
+Author	: Hoseok Lee
+Date	: 2021/01/30
+
+https://www.acmicpc.net/problem/10172
+https://github.com/hoshogi
+*/
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	printf("|\\_/| \n");
+	printf("|q p|   /} \n");
+	printf("( 0 )\"\"\"\\ \n");
+	printf("|\"^\"`    | \n");
+	printf("||_/=\\\\__|");
+
+	return 0;
+}
