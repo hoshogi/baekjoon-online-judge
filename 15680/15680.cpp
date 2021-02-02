9fd464f34786c914e3e108ae35eadba73246cc0c
hoshogi <hoshogi@gmail.com>
2021-02-03 00:08:22 +0900

    BOJ_15680: 연세대학교

---
+++ 15680/15680.cpp
@@ -0,0 +1,24 @@
+/*
+Title   : BOJ_15680 [연세대학교]
+Author  : Hoseok Lee
+Date    : 2021/02/02
+ 
+https://www.acmicpc.net/problem/15680
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int n;
+    
+    cin >> n;
+    if(n == 0)
+        cout << "YONSEI" << '\n';
+    else if(n == 1)
+        cout << "Leading the Way to the Future" << '\n';
+    
+    return 0;
+}
