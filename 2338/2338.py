8fa640fcb6b3bd1b55fa4512f7c7c2bc387119e5
hoshogi <hoshogi@gmail.com>
2021-02-07 22:06:27 +0900

    BOJ_2338: 긴자리 계산

---
+++ 2338/2338.py
@@ -0,0 +1,15 @@
+"""
+Title   : BOJ_2338 [긴자리 계산]
+Author  : Hoseok Lee
+Date    : 2021/02/07
+
+https://www.acmicpc.net/problem/2338
+https://github.com/hoshogi
+"""
+
+a = int(input())
+b = int(input())
+
+print(a+b)
+print(a-b)
+print(a*b)
