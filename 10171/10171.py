24c9f5f65613bc02f3fd231637b4fd762fd897e2
hoshogi <hoshogi@gmail.com>
2021-02-21 23:22:47 +0900

    BOJ_10171: 고양이

---
+++ 10171/10171.py
@@ -0,0 +1,16 @@
+"""
+Title:  BOJ_10171 [고양이]
+Author: Hoseok Lee
+Date:   2021/02/21
+
+https://www.acmicpc.net/problem/10171
+https://github.com/hoshogi
+"""
+
+cat = """\    /\\
+ )  ( ')
+(  /  )
+ \(__)|
+"""
+
+print(cat)
