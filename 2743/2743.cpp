5c11ebb68355b87ae66dfed5bd881ffda908bb10
hoshogi <hoshogi@gmail.com>
2021-02-22 18:29:15 +0900

    BOJ_2743: 단어 길이 재기

---
+++ 2743/2743.cpp
@@ -0,0 +1,25 @@
+/*
+Title   : BOJ_2743: 단어 길이 재기
+Author  : Hoseok Lee
+Date    : 2021/02/22
+
+https://www.acmicpc.net/problem/2743
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string word;
+    cin >> word;
+    
+    cout << word.length() << '\n';
+    
+    return 0;
+}
