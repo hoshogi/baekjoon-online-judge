1ec0627979375f65c887357b174771d1828dd3ef
hoshogi <hoshogi@gmail.com>
2021-03-02 23:20:16 +0900

    BOJ_2775: 부녀회장이 될테야

---
+++ 2775/2775.cpp
@@ -0,0 +1,44 @@
+﻿/*
+Title	: BOJ_2775 [부녀회장이 될테야]
+Author	: Hoseok Lee
+Date	: 2021/03/02
+
+https://www.acmicpc.net/problem/2775
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t;
+	int apt[15][15] = { 0 };
+
+	
+	for (int i = 1;i < 15;i++)
+		apt[0][i] = i;
+
+	for (int i = 1;i < 15;i++)
+	{
+		for (int j = 1;j < 15;j++)
+		{
+			for (int k = j;k >= 1;k--)
+				apt[i][j] += apt[i - 1][k];
+		}
+	}
+
+	cin >> t;
+	while (t--)
+	{
+		int k, n;
+
+		cin >> k >> n;
+		cout << apt[k][n] << '\n';
+	}
+
+	return 0;
+}
