f9f33302c8bd56aabf09c2eada71b74868a64b52
hoshogi <hoshogi@gmail.com>
2021-03-07 14:51:41 +0900

    BOJ_2789: 블랙잭

---
+++ 2789/2789.cpp
@@ -0,0 +1,46 @@
+/*
+Title   : BOJ_2789 [블랙잭]
+Author  : Hoseok Lee
+Date    : 2021/03/07
+
+https://www.acmicpc.net/problem/2798
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n, m;
+    int sum = 0, max = 0;
+    
+    cin >> n >> m;
+    int* card = new int [n];
+    
+    for(int i = 0;i < n;i++)
+        cin >> card[i];
+    
+    for(int i = 0;i < n;i++)
+        for(int j = 0;j < n;j++)
+            for(int k = 0;k < n;k++)
+            {
+                if (i != j && j != k && k != i)
+                {
+                    sum = card[i] + card[j] + card[k];
+                    if (sum <= m)
+                    {
+                        if (max < sum)
+                            max = sum;
+                    }
+                    
+                    sum = 0;
+                }
+            }
+    
+    cout << max << '\n';
+    return 0;
+}
