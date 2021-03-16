57361f9d1b9fec6ed54d20316b46a9dccba2f7ba
hoshogi <hoshogi@gmail.com>
2021-03-16 21:05:15 +0900

    BOJ_11718: 그대로 출력하기

---
+++ 11718/11718.cpp
@@ -0,0 +1,27 @@
+﻿/*
+Title	: BOJ_11718 [그대로 출력하기]
+Author	: Hoseok Lee
+Date	: 2021/03/16
+
+https://www.acmicpc.net/problem/11718
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string str;
+
+
+	// cin >> str 을 사용하면 공백마다 끊어서 출력한다
+	while (getline(cin, str))
+		cout << str << '\n';
+
+	return 0;
+}
