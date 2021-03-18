7b91b041fcc6a6b2aef1fa7c2baa7d94b5bcc325
hoshogi <hoshogi@gmail.com>
2021-03-18 13:19:10 +0900

    BOJ_5565: 영수증

---
+++ 5565/5565.cpp
@@ -0,0 +1,29 @@
+﻿/*
+Title	: BOJ_5565 [영수증]
+Author	: Hoseok Lee
+Date	: 2021/03/18
+
+https://www.acmicpc.net/problem/5565
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int sum, price;
+
+	cin >> sum;
+	for (int i = 0;i < 9;i++)
+	{
+		cin >> price;
+		sum -= price;
+	}
+
+	cout << sum << '\n';
+	return 0;
+}
