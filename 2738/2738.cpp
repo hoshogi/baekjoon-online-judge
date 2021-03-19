00cf0c3d2e12db0d40af5dbe8c0331fe7cea1601
hoshogi <hoshogi@gmail.com>
2021-03-19 17:50:18 +0900

    BOJ_2738: 행렬 덧셈

---
+++ 2738/2738.cpp
@@ -0,0 +1,39 @@
+﻿/*
+Title	: BOJ_2738 [행렬 덧셈]
+Author	: Hoseok Lee
+Date	: 2021/03/19
+
+https://www.acmicpc.net/problem/2738
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n, m;
+	int matrix[2][100][100]; // 100 * 100 행렬 2개 선언
+
+	cin >> n >> m;
+
+	// 행렬 2개 입력 받기
+	for (int i = 0;i < 2;i++)
+		for (int j = 0;j < n;j++)
+			for (int k = 0;k < m;k++)
+				cin >> matrix[i][j][k];
+
+	// 행렬 2개의 합 구하기
+	for (int i = 0;i < n;i++)
+	{
+		for (int j = 0;j < m;j++)
+			cout << matrix[0][i][j] + matrix[1][i][j] << ' ';
+		
+		cout << '\n';
+	}
+
+	return 0;
+}
