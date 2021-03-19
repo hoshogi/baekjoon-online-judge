cc10ab73547c726d5a1b69e4b26380f5f8ffd9f2
hoshogi <hoshogi@gmail.com>
2021-03-19 15:19:16 +0900

    BOJ_7567: 그릇

---
+++ 7567/7567.cpp
@@ -0,0 +1,33 @@
+﻿/*
+Title	: BOJ_7567 [그릇]
+Author	: Hoseok Lee
+Date	: 2021/03/19
+
+https://www.acmicpc.net/problem/7567
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int height = 10;
+	string plate;
+	
+	cin >> plate;
+	for (int i = 1;i < plate.length();i++)
+	{
+		if (plate[i - 1] != plate[i])
+			height += 10;
+		else
+			height += 5;
+	}
+
+	cout << height << '\n';
+	return 0;
+}
