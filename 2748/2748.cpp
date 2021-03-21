03a403308e362e37c361a4b72b505655cc0c3fdc
hoshogi <hoshogi@gmail.com>
2021-03-21 14:02:37 +0900

    BOJ_2748: 피보나치 수 2

---
+++ 2748/2748.cpp
@@ -0,0 +1,25 @@
+﻿/*
+Title	: BOJ_2748 [피보나치 수 2]
+Author	: Hoseok Lee
+Date	: 2021/03/21
+
+https://www.acmicpc.net/problem/2748
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n;
+	long long fib[91] = { 0, 1, }; // long long 으로 선언
+	
+	cin >> n;
+	for (int i = 2;i <= n;i++)
+		fib[i] = fib[i - 1] + fib[i - 2];
+
+	cout << fib[n] << '\n';
+
+	return 0;
+}
