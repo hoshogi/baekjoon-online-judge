c0a2d1df3fa5c1a7945018221c312fa76e4d2f9c
hoshogi <hoshogi@gmail.com>
2021-06-23 23:26:11 +0900

    BOJ_10757: 큰 수 A+B

---
+++ 10757/10757.py
@@ -0,0 +1,11 @@
+'''
+Title   : BOJ_10757 [큰 수 A+B]
+Author  : Hoseok Lee
+Date    : 2021/06/23
+
+https://www.acmicpc.net/problem/10757
+https://github.com/hoshogi
+'''
+
+a, b = input().split()
+print(int(a) + int(b))
