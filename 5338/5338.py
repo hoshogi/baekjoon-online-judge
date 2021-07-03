ec60834d4bd8898fe604923be510a7c740f47baa
hoshogi <hoshogi@gmail.com>
2021-07-03 16:51:44 +0900

    BOJ_5338: 마이크로소프트 로고

---
+++ 5338/5338.py
@@ -0,0 +1,15 @@
+"""
+Title   : BOJ_5338 [마이크로소프트 로고]
+Author  : Hoseok Lee
+Date    : 2021/07/03
+
+https://www.acmicpc.net/problem/5338
+https://github.com/hoshogi
+"""
+
+logo = """       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|"""
+print(logo)
