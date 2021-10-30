a867d251576c0ed806037f6126ecf84c4f61c537
hoshogi <hoshogi@gmail.com>
2021-10-30 21:49:45 +0900

    BOJ_16394: 홍익대학교

---
+++ 16394/Main.java
@@ -0,0 +1,18 @@
+/*
+Title   : BOJ_16394 [홍익대학교]
+Author  : Hoseok Lee
+Date    : 2021/10/30
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/16394
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int year = scanner.nextInt();
+        System.out.println(year - 1946);
+    }
+}
