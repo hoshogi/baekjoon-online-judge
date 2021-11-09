b006bb083ca8279fb1392423784858cedab306c9
hoshogi <hoshogi@gmail.com>
2021-11-09 13:44:05 +0900

    BOJ_10926: ??!

---
+++ 10926/10926.cpp
@@ -0,0 +1,22 @@
+﻿/*
+Title   : BOJ_10926 [??!]
+Author  : Hoseok Lee
+Date    : 2021/11/9
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/10926
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string id;
+
+	cin >> id;
+	cout << id + "??!\n";
+	return 0;
+}
