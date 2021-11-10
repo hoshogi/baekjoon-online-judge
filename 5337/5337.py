fbe2f15fb4f3855073ab1b5def505c07c4bbb36c
hoshogi <hoshogi@gmail.com>
2021-11-10 22:26:38 +0900

    BOJ_5337: 웰컴

---
+++ 5337/5337.py
@@ -0,0 +1,12 @@
+"""
+Title   : BOJ_5337 [웰컴]
+Author  : Hoseok Lee
+Date    : 2021/11/10
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/5337
+"""
+
+str = """.  .   .
+|  | _ | _. _ ._ _  _
+|/\|(/.|(_.(_)[ | )(/."""
+print(str)
