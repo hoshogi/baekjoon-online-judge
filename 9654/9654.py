6ef9d3a8c8ad34bdeec7204e1ecaa55d0cd855bc
hoshogi <hoshogi@gmail.com>
2021-11-16 00:31:59 +0900

    BOJ_9654: 나부 함대 데이터

---
+++ 9654/9654.py
@@ -0,0 +1,15 @@
+"""
+Title   : BOJ_9654 [나부 함대 데이터]
+Author  : Hoseok Lee
+Date    : 2021/11/16
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/9654
+"""
+
+str ="""SHIP NAME      CLASS          DEPLOYMENT IN SERVICE
+N2 Bomber      Heavy Fighter  Limited    21        
+J-Type 327     Light Combat   Unlimited  1         
+NX Cruiser     Medium Fighter Limited    18        
+N1 Starfighter Medium Fighter Unlimited  25        
+Royal Cruiser  Light Combat   Limited    4"""
+print(str)
