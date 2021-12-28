8a94e6ccdd562b33c6133c1d44f8e1de6370d17b
hoshogi <hoshogi@gmail.com>
2021-12-28 21:46:17 +0900

    BOJ_10170: NFC West vs North

---
+++ 10170/10170.py
@@ -0,0 +1,22 @@
+"""
+Title   : BOJ_10170 [NFC West vs North]
+Author  : Hoseok Lee
+Date    : 2021/12/28
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/10170
+"""
+
+str = """NFC West       W   L  T
+-----------------------
+Seattle        13  3  0
+San Francisco  12  4  0
+Arizona        10  6  0
+St. Louis      7   9  0
+
+NFC North      W   L  T
+-----------------------
+Green Bay      8   7  1
+Chicago        8   8  0
+Detroit        7   9  0
+Minnesota      5  10  1"""
+print(str)
