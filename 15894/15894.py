cf57fdf79584c4e4246c1bfe58f0965a1c71387e
hoshogi <hoshogi@gmail.com>
2021-12-31 23:40:45 +0900

    BOJ_15894 :수학은 체육과목 입니다

---
+++ 15894/15894.py
@@ -0,0 +1,10 @@
+"""
+Title   : BOJ_15894 [수학은 체육과목 입니다]
+Author  : Hoseok Lee
+Date    : 2021/12/31
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/15894
+"""
+
+num = input()
+print(str(4 * int(num)))
