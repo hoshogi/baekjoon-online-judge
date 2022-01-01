789a0b5c3d4192e7bad436a36168f442b0064ba9
hoshogi <hoshogi@gmail.com>
2022-01-01 22:26:16 +0900

    BOJ_15740: A+B - 9

---
+++ 15740/15740.py
@@ -0,0 +1,11 @@
+"""
+Title   : BOJ_15740 [A+B - 9]
+Author  : Hoseok Lee
+Date    : 2022/01/01
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/15740
+"""
+
+nums = input()
+num = nums.split()
+print(int(num[0]) + int(num[1]))
