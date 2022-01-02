65eb975200bd0c531256b16ef08a2d40493250e1
hoshogi <hoshogi@gmail.com>
2022-01-02 23:34:58 +0900

    BOJ_13277: 큰 수 곱셈

---
+++ 13277/13277.py
@@ -0,0 +1,11 @@
+"""
+Title   : BOJ_13277 [큰 수 곱셈]
+Author  : Hoseok Lee
+Date    : 2022/01/02
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/13277
+"""
+
+nums = input()
+num = nums.split()
+print(int(num[0]) * int(num[1]))
