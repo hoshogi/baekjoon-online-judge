d9eb7985263bbdab59cc1da01b95676712a3c03d
hoshogi <hoshogi@gmail.com>
2022-01-03 16:09:07 +0900

    BOJ_14928: 큰 수 (BIG)

---
+++ 14928/14928.py
@@ -0,0 +1,10 @@
+"""
+Title   : BOJ_14928 [큰 수 (BIG)]
+Author  : Hoseok Lee
+Date    : 2022/01/03
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/14928
+"""
+
+num = input()
+print(int(num) % 20000303)
