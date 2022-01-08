7dd5f5d47c66a153bc75099ab7e76cf270130b38
hoshogi <hoshogi@gmail.com>
2022-01-08 23:00:21 +0900

    BOJ_18108: 1998년생인 내가 태국에서는 2541년생?!

---
+++ 18108/18108.py
@@ -0,0 +1,10 @@
+"""
+Title   : BOJ_18108 [1998년생인 내가 태국에서는 2541년생?!]
+Author  : Hoseok Lee
+Date    : 2022/01/08
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/18108
+"""
+
+year = int(input())
+print(year - 543)
