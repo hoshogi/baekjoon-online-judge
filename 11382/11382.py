aae88de31ad7080e3f065fe6370c8d2b75d9227d
hoshogi <hoshogi@gmail.com>
2022-01-09 13:38:54 +0900

    BOJ_11382: 꼬마 정민

---
+++ 11382/11382.py
@@ -0,0 +1,10 @@
+"""
+Title   : BOJ_11382 [꼬마 정민]
+Author  : Hoseok Lee
+Date    : 2022/01/09
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/11382
+"""
+
+a, b, c = map(int, input().split())
+print(a + b + c)
