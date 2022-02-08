858a7600a28e5cc05d22e8dc1bbee1d49e812dfb
hoshogi <hoshogi@gmail.com>
2022-02-08 22:53:28 +0900

    BOJ_5339: 콜센터

---
+++ 5339/5339.py
@@ -0,0 +1,21 @@
+"""
+Title   : BOJ_5339 [콜센터]
+Author  : Hoseok Lee
+Date    : 2022/02/08
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/5339
+"""
+
+str = """     /~\\
+    ( oo|
+    _\=/_
+   /  _  \\
+  //|/.\|\\\\
+ ||  \ /  ||
+============
+|          |
+|          |
+|          |"""
+
+print(str)
+
