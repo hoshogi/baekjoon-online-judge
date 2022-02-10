c9c48a4d20f91c94fa76eb90401e4764066c1a96
hoshogi <hoshogi@gmail.com>
2022-02-10 16:19:53 +0900

    BOJ_14645: 와이버스 부릉부릉

---
+++ 14645/14645.py
@@ -0,0 +1,14 @@
+"""
+Title   : BOJ_14645 [와이버스 부릉부릉]
+Author  : Hoseok Lee
+Date    : 2022/02/10
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/14645
+"""
+
+n, k = map(int, input().split())
+
+for _ in range(n):
+    up, down = map(int, input().split())
+
+print("비와이")
