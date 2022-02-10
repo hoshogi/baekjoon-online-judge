06a9c6777356c70a3ee871919d471a369dffe9e5
hoshogi <hoshogi@gmail.com>
2022-02-10 16:24:31 +0900

    BOJ_9316: Hello Judge

---
+++ 9316/9316.py
@@ -0,0 +1,12 @@
+"""
+Title   : BOJ_9316 [Hello Judge]
+Author  : Hoseok Lee
+Date    : 2022/02/10
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/9316
+"""
+
+n = int(input())
+
+for i in range(n):
+    print(f"Hello World, Judge {i+1}!")
