7eb7f9e09ec451c91dc2e396e4f4b0b775531900
hoshogi <hoshogi@gmail.com>
2022-02-10 17:25:03 +0900

    BOJ_16486: 운동장 한 바퀴

---
+++ 16486/16486.py
@@ -0,0 +1,14 @@
+"""
+Title   : BOJ_16486 [운동장 한 바퀴]
+Author  : Hoseok Lee
+Date    : 2022/02/10
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/16486
+"""
+
+PI = 3.141592
+d1 = int(input())
+d2 = int(input())
+
+result = 2 * d1 + 2 * PI * d2
+print(result)
