f0ff734e0fe1f1b1315c0073760776a288d81692
hoshogi <hoshogi@gmail.com>
2022-02-11 16:59:59 +0900

    BOJ_15727: 조별과제를 하려는데 조장이 사라졌다

---
+++ 15727/15727.py
@@ -0,0 +1,12 @@
+"""
+Title   : BOJ_15727 [조별과제를 하려는데 조장이 사라졌다]
+Author  : Hoseok Lee
+Date    : 2022/02/11
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/15727
+"""
+
+import math
+
+num = int(input())
+print(math.ceil(num / 5))
