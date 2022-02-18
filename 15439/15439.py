276c9345970f5c0e4803678ea18672b49a801ee4
hoshogi <hoshogi@gmail.com>
2022-02-18 13:25:38 +0900

    BOJ_15439: Vera and Outfits

---
+++ 15439/15439.py
@@ -0,0 +1,11 @@
+"""
+Title   : BOJ_15439 [Vera and Outfits]
+Author  : Hoseok Lee
+Date    : 2022/02/18
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/15439
+"""
+
+n = int(input())
+
+print(n * (n - 1))
