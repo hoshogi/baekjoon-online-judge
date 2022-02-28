4ce5f42b8e2fb7d508c6fb3441f6975cd779fca0
hoshogi <hoshogi@gmail.com>
2022-02-28 21:01:12 +0900

    BOJ_15963: CASIO

---
+++ 15963/15963.py
@@ -0,0 +1,14 @@
+"""
+Title   : BOJ_15963 [CASIO]
+Author  : Hoseok Lee
+Date    : 2022/02/28
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/15963
+"""
+
+n, m = map(int, input().split())
+
+if n == m:
+    print(1)
+else:
+    print(0)
