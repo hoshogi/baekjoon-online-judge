7988f58234ee4a10f95d9e8d2f3473b274438d38
hoshogi <hoshogi@gmail.com>
2022-03-09 15:34:34 +0900

    BOJ_2847: 게임을 만든 동준이

---
+++ 2847/2847.cpp
@@ -0,0 +1,37 @@
+﻿/*
+Title   : BOJ_2847 [게임을 만든 동준이]
+Author  : Hoseok Lee
+Date    : 2022/03/09
+https://www.acmicpc.net/problem/2847
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	int n, result = 0;
+	
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++)
+		cin >> v[i];
+
+	int temp = v[n - 1];
+	for (int i = n - 2; i >= 0; i--) {
+		if (v[i] >= temp) {
+			result += v[i] - temp + 1;
+			temp--;
+		}
+		else {
+			temp = v[i];
+		}
+	}
+	
+	cout << result << '\n';
+	return 0;
+}
