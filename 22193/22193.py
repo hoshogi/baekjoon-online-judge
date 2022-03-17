eed0dfe3ac96ec4caa57b88fe11e582a00b967a8
hoshogi <hoshogi@gmail.com>
2022-03-17 23:52:59 +0900

    BOJ_22193: Multiply

---
+++ 22193/22193.py
@@ -0,0 +1,12 @@
+"""
+Title   : BOJ_22193 [Multiply]
+Author  : Hoseok Lee
+Date    : 2022/03/17
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/22193
+"""
+
+n, m = map(int, input().split())
+a = int(input())
+b = int(input())
+print(a * b)
