977847f3063f4dd6a475d4c2f8b1d4ef05983acc
hoshogi <hoshogi@gmail.com>
2022-03-25 22:21:07 +0900

    BOJ_9372: 상근이의 여행

---
+++ 9372/9372.cpp
@@ -0,0 +1,31 @@
+/*
+Title   : BOJ_9372 [상근이의 여행]
+Author  : Hoseok Lee
+Date    : 2022/03/25
+https://www.acmicpc.net/problem/9372
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int t;
+    
+    cin >> t;
+    while (t--) {
+        int n, m;
+        
+        cin >> n >> m;
+        for (int i = 0; i < m; i++) {
+            int a, b;
+            
+            cin >> a >> b;
+        }
+        cout << n - 1 << '\n';
+    }
+    return 0;
+}
