ef1770775ae75848383c336d98b9448be17bfb9c
hoshogi <hoshogi@gmail.com>
2022-05-07 23:30:40 +0900

    BOJ_13706: 제곱근

---
+++ 13706/13706.py
@@ -0,0 +1,12 @@
+"""
+Title   : BOJ_13706 [제곱근]
+Author  : Hoseok Lee
+Date    : 2022/05/07
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/13706
+"""
+
+import math
+
+n = int(input())
+print(math.isqrt(n))
