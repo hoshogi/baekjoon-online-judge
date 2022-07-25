89eb16d3571d3fbc0fff3919d138d62b1af6d8a5
hoshogi <hoshogi@gmail.com>
2022-07-25 22:17:53 +0900

    BOJ_2193: 이친수

--- 2193/2193.cpp
+++ 2193/2193.cpp
@@ -1,7 +1,7 @@
 /*
 Title   : BOJ_2193 [이친수]
 Author  : Hoseok Lee
-Date    : 2022/02/24
+Date    : 2022/07/25
 https://www.acmicpc.net/problem/2193
 https://github.com/hoshogi
 */
@@ -12,19 +12,18 @@ using namespace std;
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+    cout.tie(nullptr);
     
     int n;
-    long long zero[91] = {0, 0};
-    long long one[91] = {0, 1};
-    long long result[91] = {0, 1};
-    
-    for (int i = 2; i <= 90; i++) {
-        zero[i] = zero[i - 1] + one[i - 1];
-        one[i] = zero[i - 1];
-        result[i] = zero[i] + one[i];
-    }
+    long long d[91][2] = { {0, 0},
+                     {0, 1} };
     
     cin >> n;
-    cout << result[n] << '\n';
+    for (int i = 2; i <= n; i++) {
+        d[i][0] = d[i - 1][0] + d[i - 1][1];
+        d[i][1] = d[i - 1][0];
+    }
+    
+    cout << d[n][0] + d[n][1] << '\n';
     return 0;
 }
