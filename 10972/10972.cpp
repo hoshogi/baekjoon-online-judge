20507f69d8419848ef54b739a1643c926e805181
hoshogi <hoshogi@gmail.com>
2022-09-24 00:17:41 +0900

    BOJ_10972: 다음 순열

---
+++ 10972/10972.cpp
@@ -0,0 +1,37 @@
+/*
+Title   : BOJ_10972 [다음 순열]
+Author  : Hoseok Lee
+Date    : 2022/09/24
+https://www.acmicpc.net/problem/10972
+https://github.com/hoshogi
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    int n;
+    vector<int> v;
+    
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int num;
+        
+        cin >> num;
+        v.push_back(num);
+    }
+    
+    if (next_permutation(v.begin(), v.end())) {
+        for (auto it : v)
+            cout << it << ' ';
+    }
+    else
+        cout << -1 << '\n';
+    return 0;
+}
