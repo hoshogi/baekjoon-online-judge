66c7ae109d84758bf89ca4d8f6282cc3d984f12b
hoshogi <hoshogi@gmail.com>
2023-01-20 14:50:39 +0900

    BOJ_25083: 새싹

---
+++ 25083/25083.py
@@ -0,0 +1,15 @@
+"""
+Title   : BOJ_25083 [새싹]
+Author  : Hoseok Lee
+Date    : 2023/01/20
+https://github.com/hoshogi
+https://www.acmicpc.net/problem/25083
+"""
+
+str ="""         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |"""
+print(str)
